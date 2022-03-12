37e6948bd69f1550741f9567e0118ad200e31954
Rajukst <rajuahamedkst15@gmail.com>
2022-03-12 12:44:12 +0600

    added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const { MongoClient } = require("mongodb");
 const app = express();
 require("dotenv").config();
-const port = process.env.PORT || 6000;
+const port = process.env.PORT || 5000;
 const ObjectId = require("mongodb").ObjectId;
 app.use(cors());
 app.use(express.json());
